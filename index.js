a2feff31a5b3ae8790952a34ac085ecc8d0b3177
medkhalilbk <medkhalilbenkhelil@gmail.com>
2023-12-05 00:19:34 +0100

    test for auto deploy

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ app.get('/test', function (req, res) {
     res.send('{ "response": "Hello World" }');
 });
 app.get('/mpdam', function (req, res) {
-    res.send('{ "response": "It works !" }');
+    res.send('{ "response": "It works ! " }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
