cc9b7d5eef0a7900503d9c18c3a8f16fb76f4f89
Med Khalil Benkhelil <medkhalilbenkhelil@gmail.com>
2023-12-05 10:54:04 +0100

    test 3

--- test.js
+++ test.js
@@ -1,15 +1,22 @@
 const request = require('supertest');
-const {expect} = require('expect');
+const { expect } = require('expect');
 
 const app = require('./index');
 
 describe('Testing POSTS/answers endpoint', () => {
-  it('respond with valid HTTP status code and description and message', async () => { // add `async` keyword here
+  it('respond with valid HTTP status code and description and message', async () => {
     const response = await request(app)
       .post('/postMPDAM')
-      .send({ data:"test" });
+      .send({ data: 'test' });
 
     expect(response.status).toBe(403);
-    expect(response.body.data).toBe("Auth Needed" ); 
+    expect(response.body.data).toBe('Auth Needed');
+  });
+
+  // Add an afterAll hook to shut down the server
+  afterAll(() => {
+    // Assuming your server has a method to gracefully shut down
+    // Replace the following line with the actual method to shut down your server
+    app.close();
   });
 });
