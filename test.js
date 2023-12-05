5fc48a82cb268af4d1ec1d70c854ebfdeaea08e5
Med Khalil Benkhelil <medkhalilbenkhelil@gmail.com>
2023-12-05 11:03:30 +0100

    test 5

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ describe('Testing POSTS/answers endpoint', () => {
   it('respond with valid HTTP status code and description and message', async () => {
     const response = await request(app)
       .post('/postMPDAM')
-      .send({ data: 'test' });
+      .send({ data: 'test'  });
 
     expect(response.status).toBe(403);
     expect(response.body.data).toBe('Auth Needed');
